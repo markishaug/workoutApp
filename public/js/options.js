22810326235b5791fce7bda926825461f28cc81e
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-09-01 14:53:30 -0600

    fixed a typo in newUser and started optionjs

---
+++ public/js/options.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+var workoutOptions = {
+    time: $("#time").val(),
+    purpose: $("#purpose").val(),
+    muscleGroup: $("#mg").val(),
+    equipment: $("#equipment").val()
+};
+
+// Pick workout based on values
+if (workoutOptions.time === time1) {
+    time1Workout();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
