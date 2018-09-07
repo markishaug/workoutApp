dcb2412a7abadcb0c029c45c8ad6931a3c07279a
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-09-07 12:01:07 -0600

    updated exercise-list seeds to match the model

--- public/js/options.js
+++ public/js/options.js
@@ -4,12 +4,7 @@ $(options).on("submit", handleFormSubmit);
 
 function handleFormSubmit(event) {
     event.preventDefault();
-// Wont submit the post if we are missing time, purpose, mg, or equipment. Will set a none option for equipment
-
-
-
-
-// todayOptions();
+todayOptions();
 // Create constructor for options table in database
 var workoutOptions = {
     time: $("input[type=checkbox][name=time]:checked").val(),
@@ -17,31 +12,34 @@ var workoutOptions = {
     muscleGroup: $("input[type=checkbox][name=mg]:checked").val(),
     equipment: $("input[type=checkbox][name=equipment]:checked").val()
 };
-
+// Wont submit the post if we are missing time, purpose, mg, or equipment. Will set a none option for equipment
 if (!workoutOptions.time || !workoutOptions.purpose || !workoutOptions.muscleGroup || !workoutOptions.equipment) {
     return;
   }
-console.log(workoutOptions);
-console.log(workoutOptions.time);
-// Post choices to wod api
-// function todayOptions() {
-//     $.post("/api/wod", workoutOptions)
-//     .then(timeOption);
-// };
 
+// Post choices to wod api
+function todayOptions() {
+    $.post("/api/exercises", workoutOptions)
+    .then(timeOption);
+};
+console.log(workoutOptions.muscleGroup);
 timeOption();
 
 // Get values and run functions based on chosen values
 
 // Pick workout based on values
 function timeOption() {
+    console.log(workoutOptions.time);
 // Time value decides how many exercise are chosen
 var exerciseArr = [];
     $.get("/api/exercises", function(data) {
+        console.log(data);
         for(var i = 0; i < data.length; i++) {
             var exerciseCount = data[i];
         if (workoutOptions.time === "time1") {
             // pick 2 
+            console.log(workoutOptions.time);
+            console.log(exerciseCount);
         }
         exerciseArr.push(exerciseCount);
         };
