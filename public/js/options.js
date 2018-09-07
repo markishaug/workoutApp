51501271ae9879b47066ed43a34b4e37eef14cb9
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-09-07 17:33:38 -0600

    fixed all the nonsense from options.js

--- public/js/options.js
+++ public/js/options.js
@@ -4,7 +4,7 @@ $(options).on("submit", handleFormSubmit);
 
 function handleFormSubmit(event) {
     event.preventDefault();
-todayOptions();
+
 // Create constructor for options table in database
 var workoutOptions = {
     time: $("input[type=checkbox][name=time]:checked").val(),
@@ -17,66 +17,16 @@ if (!workoutOptions.time || !workoutOptions.purpose || !workoutOptions.muscleGro
     return;
   }
 
-// Post choices to wod api
-function todayOptions() {
-    $.post("/api/exercises", workoutOptions)
-    .then(timeOption);
-};
-console.log(workoutOptions.muscleGroup);
-timeOption();
+  $.post("/api/process/bestexercise", workoutOptions, function(data) {
+    console.log(data);
+  });
 
-// Get values and run functions based on chosen values
-
-// Pick workout based on values
-function timeOption() {
-    console.log(workoutOptions.time);
-// Time value decides how many exercise are chosen
-var exerciseArr = [];
-    $.get("/api/exercises", function(data) {
-        console.log(data);
-        for(var i = 0; i < data.length; i++) {
-            var exerciseCount = data[i];
-        if (workoutOptions.time === "time1") {
-            // pick 2 
-            console.log(workoutOptions.time);
-            console.log(exerciseCount);
-        }
-        exerciseArr.push(exerciseCount);
-        };
-    });
-
-    purposeOption();
 
 };
-// Purpose decides sets, reps, weight
-function purposeOption() {
 
-}
-
-// Muscle group runs through exercises and picks those that are true based on values chosen
-
-// Equipment runs thorugh exercises and picks those where eq value equals true
 
 // Function that gets the entire workout and renders it to workout page
 function wod() {
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,17 @@ module.exports = function(app) {
             res.json(dblifting);
         });
     });
+    app.post("/api/process/bestexercise", function(req, res) {
+        console.log("------------------------------",req.body);
+        db.ExerciseList.findAll({}).then(function(dblifting) {
+            var choices = req.body;
+           if (choices.time === "time1") {
+            //    pick 2
+            
+           }
+            res.json(dblifting);
+        });
+    })
     //Get route for getting all of bodyweight exercises
     app.get("/api/exercises/bodyweight", function(req, res) {
         db.BodyWeight.findAll({}).then(function(dblifting) {
@@ -19,6 +30,17 @@ module.exports = function(app) {
     });
     //Post route for adding a new exercise
     app.post("/api/exercises", function(req, res) {
+        var test = {
+            name: "squats",
+            posterior: true,
+            back: true,
+            biceps: false,
+            triceps: false,
+            shoulders: false,
+            chest: false,
+            legs: true,
+            barbell: true
+        }
         db.ExerciseList.create(req.body).then(function(dblifting) {
             res.json(dblifting)
         }).catch(function(err) {
