d487db176def2b722877f9132adeedbc2a9254a5
Robert <35673753+smitty10010@users.noreply.github.com>
2018-09-08 12:23:48 -0600

    the config js file for the db

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports = {
+    development: {
+        username: process.env.DEVELOPMENT_USERNAME,
+        password: process.env.DEVELOPMENT_PASSWORD,
+        database: process.env.DEVELOPMENT_DATABASE,
+        host: "127.0.0.1",
+        dialect: "mysql"
+    },
+    test: {
+        username: process.env.TEST_USERNAME,
+        password: process.env.TEST_PASSWORD,
+        database: process.env.TEST_DATABASE,
+        host: "127.0.0.1",
+        dialect: "mysql"
+    },
+    production: {
+        username: process.env.PRODUCTION_USERNAME,
+        password: process.env.PRODUCTION_PASSWORD,
+        database: process.env.PRODUCTION_DATABASE,
+        host: "127.0.0.1",
+        dialect: "mysql"
+    }
+};
