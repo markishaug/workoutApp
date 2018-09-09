25afa518eddd48f88f592103fef598761510727b
Robert <35673753+smitty10010@users.noreply.github.com>
2018-09-09 17:36:19 -0600

    api route to return workout to user. includes body weight and exercise

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,57 +12,409 @@ module.exports = function(app) {
     });
     // POST route to run through the exercises and pick through based on user choices
     app.post("/api/process/bestexercise", function(req, res) {
+            //An empty var for assigning an array to later    
             var muscleGroup;
+            //assigning muscleGroup values to variable for easy access
             var muscleGroupRequest = req.body.muscleGroup
-            console.log("------------------------------", muscleGroupRequest);
-            console.log(Array.isArray(muscleGroupRequest));
+                //checks to see if the property of muscleGroupRequest is an array
             if (Array.isArray(muscleGroupRequest)) {
                 muscleGroup = [];
+                //parseses out each muscle group from the request and assigns it to the obj Object as a property with the value of true. Then pushes obj into muscleGroup
                 for (i = 0; i < muscleGroupRequest.length; i++) {
                     obj = {};
                     obj[muscleGroupRequest[i]] = true;
                     muscleGroup.push(obj);
                 }
+                //if the property of muscleGroupRequest is not an array then it creates an Object and asseigns the muscleGroup as a property with a value of true.
             } else {
                 muscleGroup = {};
                 muscleGroup[muscleGroupRequest] = true;
             }
-            console.log("----------------------------", muscleGroup);
+            //Sequalize for exercises where selected muscleGroup have the property values of true
             db.ExerciseList.findAll({
                 where: {
                     $or: muscleGroup
                 }
             }).then(function(dblifting) {
-                res.json(dblifting);
+                //var for time in the request
+                time = req.body.time;
+                //blank array variable that will be returend to user once array is populated
+                workoutArray = [];
+                //else if statements to decided how many exercises to return to user
+                if (time === 2) {
+                    //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+                    for (i = 1; i <= 1; i++) {
+                        var compound;
+
+                        function findCompound(exercise) {
+                            if (exercise.compound === true) {
+                                return true;
+                            };
+                        };
+                        compound = dblifting.filter(findCompound);
+
+                        function getRandomCompoundIndex() {
+                            min = 0;
+                            max = compound.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                    }
+                    //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                    for (i = 1; i <= 1; i++) {
+                        var accessory;
+
+                        function findAccessory(exercise) {
+                            if (exercise.accessory === true) {
+                                return true;
+                            };
+                        };
+
+                        accessory = dblifting.filter(findAccessory);
+
+                        function getRandomAccessoryIndex() {
+                            min = 0;
+                            max = accessory.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                    }
+
+                } else if (time === 4) {
+                    //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+
+                    for (i = 1; i <= 1; i++) {
+                        var compound;
+
+                        function findCompound(exercise) {
+                            if (exercise.compound === true) {
+                                return true;
+                            };
+                        };
+                        compound = dblifting.filter(findCompound);
+
+                        function getRandomCompoundIndex() {
+                            min = 0;
+                            max = compound.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                    }
+                    //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                    for (i = 1; i <= 3; i++) {
+                        var accessory;
+
+                        function findAccessory(exercise) {
+                            if (exercise.accessory === true) {
+                                return true;
+                            };
+                        };
+
+                        accessory = dblifting.filter(findAccessory);
+
+                        function getRandomAccessoryIndex() {
+                            min = 0;
+                            max = accessory.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                    }
+                } else if (time === 6) {
+                    //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+
+                    for (i = 1; i <= 1; i++) {
+                        var compound;
+
+                        function findCompound(exercise) {
+                            if (exercise.compound === true) {
+                                return true;
+                            };
+                        };
+                        compound = dblifting.filter(findCompound);
+
+                        function getRandomCompoundIndex() {
+                            min = 0;
+                            max = compound.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                    }
+                    //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                    for (i = 1; i <= 5; i++) {
+                        var accessory;
+
+                        function findAccessory(exercise) {
+                            if (exercise.accessory === true) {
+                                return true;
+                            };
+                        };
+
+                        accessory = dblifting.filter(findAccessory);
+
+                        function getRandomAccessoryIndex() {
+                            min = 0;
+                            max = accessory.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                    }
+                } else if (time === 8) {
+                    //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+
+                    for (i = 1; i <= 1; i++) {
+                        var compound;
+
+                        function findCompound(exercise) {
+                            if (exercise.compound === true) {
+                                return true;
+                            };
+                        };
+                        compound = dblifting.filter(findCompound);
+
+                        function getRandomCompoundIndex() {
+                            min = 0;
+                            max = compound.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                    }
+                    //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                    for (i = 1; i <= 7; i++) {
+                        var accessory;
+
+                        function findAccessory(exercise) {
+                            if (exercise.accessory === true) {
+                                return true;
+                            };
+                        };
+
+                        accessory = dblifting.filter(findAccessory);
+
+                        function getRandomAccessoryIndex() {
+                            min = 0;
+                            max = accessory.length;
+                            return Math.floor(Math.random() * (max - min + 1)) + min;
+                        };
+
+                        workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                    }
+                }
+                //returns to user the requested workouts
+                res.json(workoutArray);
             });
         })
         // POST route to run through the exercises and pick through based on user choices
     app.post("/api/process/bestbodyweightexercise", function(req, res) {
-            var muscleGroup;
-            var muscleGroupRequest = req.body.muscleGroup
-            console.log("------------------------------", muscleGroupRequest);
-            console.log(Array.isArray(muscleGroupRequest));
-            if (Array.isArray(muscleGroupRequest)) {
-                muscleGroup = [];
-                for (i = 0; i < muscleGroupRequest.length; i++) {
-                    obj = {};
-                    obj[muscleGroupRequest[i]] = true;
-                    muscleGroup.push(obj);
-                }
-            } else {
-                muscleGroup = {};
-                muscleGroup[muscleGroupRequest] = true;
+        var muscleGroup;
+        var muscleGroupRequest = req.body.muscleGroup
+        if (Array.isArray(muscleGroupRequest)) {
+            muscleGroup = [];
+            for (i = 0; i < muscleGroupRequest.length; i++) {
+                obj = {};
+                obj[muscleGroupRequest[i]] = true;
+                muscleGroup.push(obj);
             }
-            console.log("----------------------------", muscleGroup);
-            db.BodyWeight.findAll({
-                where: {
-                    $or: muscleGroup
+        } else {
+            muscleGroup = {};
+            muscleGroup[muscleGroupRequest] = true;
+        }
+        db.BodyWeight.findAll({
+            where: {
+                $or: muscleGroup
+            }
+        }).then(function(dblifting) {
+            //var for time in the request
+            time = req.body.time;
+            //blank array variable that will be returend to user once array is populated
+            workoutArray = [];
+            //else if statements to decided how many exercises to return to user
+            if (time === 2) {
+                //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+                for (i = 1; i <= 1; i++) {
+                    var compound;
+
+                    function findCompound(exercise) {
+                        if (exercise.compound === true) {
+                            return true;
+                        };
+                    };
+                    compound = dblifting.filter(findCompound);
+
+                    function getRandomCompoundIndex() {
+                        min = 0;
+                        max = compound.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(compound[getRandomCompoundIndex()]);
+
                 }
-            }).then(function(dblifting) {
-                res.json(dblifting);
-            });
-        })
-        //Get route for getting all of bodyweight exercises
+                //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                for (i = 1; i <= 1; i++) {
+                    var accessory;
+
+                    function findAccessory(exercise) {
+                        if (exercise.accessory === true) {
+                            return true;
+                        };
+                    };
+
+                    accessory = dblifting.filter(findAccessory);
+
+                    function getRandomAccessoryIndex() {
+                        min = 0;
+                        max = accessory.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                }
+
+            } else if (time === 4) {
+                //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+
+                for (i = 1; i <= 1; i++) {
+                    var compound;
+
+                    function findCompound(exercise) {
+                        if (exercise.compound === true) {
+                            return true;
+                        };
+                    };
+                    compound = dblifting.filter(findCompound);
+
+                    function getRandomCompoundIndex() {
+                        min = 0;
+                        max = compound.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                }
+                //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                for (i = 1; i <= 3; i++) {
+                    var accessory;
+
+                    function findAccessory(exercise) {
+                        if (exercise.accessory === true) {
+                            return true;
+                        };
+                    };
+
+                    accessory = dblifting.filter(findAccessory);
+
+                    function getRandomAccessoryIndex() {
+                        min = 0;
+                        max = accessory.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                }
+            } else if (time === 6) {
+                //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+
+                for (i = 1; i <= 1; i++) {
+                    var compound;
+
+                    function findCompound(exercise) {
+                        if (exercise.compound === true) {
+                            return true;
+                        };
+                    };
+                    compound = dblifting.filter(findCompound);
+
+                    function getRandomCompoundIndex() {
+                        min = 0;
+                        max = compound.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                }
+                //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                for (i = 1; i <= 5; i++) {
+                    var accessory;
+
+                    function findAccessory(exercise) {
+                        if (exercise.accessory === true) {
+                            return true;
+                        };
+                    };
+
+                    accessory = dblifting.filter(findAccessory);
+
+                    function getRandomAccessoryIndex() {
+                        min = 0;
+                        max = accessory.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                }
+            } else if (time === 8) {
+                //of the availbe exercises one compound exercise is selected at random and pushed into workoutArray
+
+                for (i = 1; i <= 1; i++) {
+                    var compound;
+
+                    function findCompound(exercise) {
+                        if (exercise.compound === true) {
+                            return true;
+                        };
+                    };
+                    compound = dblifting.filter(findCompound);
+
+                    function getRandomCompoundIndex() {
+                        min = 0;
+                        max = compound.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(compound[getRandomCompoundIndex()]);
+
+                }
+                //the rest of the workoutArray is filled with accessory exercises based off of the total amount of exercises the user is requesting
+                for (i = 1; i <= 7; i++) {
+                    var accessory;
+
+                    function findAccessory(exercise) {
+                        if (exercise.accessory === true) {
+                            return true;
+                        };
+                    };
+
+                    accessory = dblifting.filter(findAccessory);
+
+                    function getRandomAccessoryIndex() {
+                        min = 0;
+                        max = accessory.length;
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+
+                    workoutArray.push(accessory[getRandomAccessoryIndex()]);
+                }
+            }
+
+
+            //returns to user the requested workouts
+            res.json(workoutArray);
+        });
+    });
+    //Get route for getting all of bodyweight exercises
     app.get("/api/exercises/bodyweight", function(req, res) {
         db.BodyWeight.findAll({}).then(function(dblifting) {
             res.json(dblifting);
