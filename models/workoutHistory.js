e5b5ff3641146d1cd806f6361d8a21d614dc2534
Robert <35673753+smitty10010@users.noreply.github.com>
2018-09-09 15:01:39 -0600

    Corrected DATATYPES to DataTypes

--- models/workoutHistory.js
+++ models/workoutHistory.js
@@ -1,11 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
     var WorkoutHistory = sequelize.define("WorkoutHistory", {
-        date: DATATYPE.DATE
+        date: DataTypes.DATE
     });
 
     WorkoutHistory.associate = function(models) {
         // Associating WorkoutHistory with UserWorkouts
         WorkoutHistory.hasMany(models.UserWorkout);
-      };
-return WorkoutHistory;
+    };
+    return WorkoutHistory;
 };
