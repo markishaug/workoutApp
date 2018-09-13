c997a3517f5786ce08788b492320a6adc1311c61
Robert <35673753+smitty10010@users.noreply.github.com>
2018-09-12 18:38:42 -0600

    test data

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,4 +1,8 @@
 //require chart constructor
 var Chart = require('chart.js');
 //create new chart object for the user based off of their workout history
-var userProfileChart = new Chart(ctx, {});
+var userProfileChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+});
