0a2f73bdb8b593fd7ad0d39644395ed61ba26bad
Robert <35673753+smitty10010@users.noreply.github.com>
2018-09-13 23:02:02 -0600

    added fake chart for now

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,8 +1,33 @@
+// $(document).ready(function() {
+//     var id = sessionStorage.getItem("id");
+//     $.post("/api/workoutHistory", id, function(res) {
+
+//     });
+
+// });
+
 //require chart constructor
-var Chart = require('chart.js');
+// var Chart = require('chart.js');
 //create new chart object for the user based off of their workout history
-var userProfileChart = new Chart(ctx, {
+new Chart(document.getElementById("chart"), {
     type: 'line',
-    data: data,
-    options: options
+    data: {
+        labels: ["Jan", "Feb", "March", "April", "May", "June"],
+        datasets: [{
+            label: "My First Dataset",
+            data: [
+                65,
+                59,
+                80,
+                81,
+                56,
+                55,
+                40
+            ],
+            fill: false,
+            // borderColor: rgb(75, 192, 192),
+            lineTension: 0.1
+        }]
+    },
+    options: {}
 });
